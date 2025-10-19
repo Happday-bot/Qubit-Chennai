a11b76196eaac2b40e3a47e7c8b174900289f438
Alfred Sam <alfredsam2006@gmail.com>
2025-10-19 15:22:16 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8000', // Update this to your production API URL
+  apiUrl: 'https://qubitbackend.onrender.com', // Update this to your production API URL
 };
